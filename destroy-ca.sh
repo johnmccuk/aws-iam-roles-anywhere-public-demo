275b79041bc58910883fc0ecc2f1d2527d060720
John McCracken <johnmccuk@gmail.com>
2025-08-24 14:30:15 +0100

    add destroy-ca.sh

---
+++ destroy-ca.sh
@@ -0,0 +1,13 @@
+rm ~/.ssh/iam-roles-anywhere-demo.*
+
+rm root-ca/private/root-ca.key
+rm root-ca/private/root-ca.csr
+rm root-ca/private/root-ca.crt
+
+rm root-ca/certs/*.pem
+
+rm root-ca/clients/client.key
+rm root-ca/clients/client.csr
+rm root-ca/clients/client.crt
+
+rm root-ca/db/*
